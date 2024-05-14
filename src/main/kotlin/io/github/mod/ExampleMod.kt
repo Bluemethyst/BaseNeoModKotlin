42a8cda22208db5bcbd669ad98eb83ce14399568
blue <bluemethyst@gmail.com>
2024-05-14 18:34:20 +1200

    Changed "Mod" to "ExampleMod"

--- src/main/kotlin/io/github/mod/ExampleMod.kt
+++ src/main/kotlin/io/github/mod/ExampleMod.kt
@@ -3,12 +3,13 @@ package io.github.mod
 import com.mojang.logging.LogUtils
 import net.neoforged.bus.api.IEventBus
 import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
 import org.slf4j.Logger
 
-@net.neoforged.fml.common.Mod(Mod.MODID)
-class Mod(modEventBus: IEventBus?, modContainer: ModContainer?) {
+@Mod(ExampleMod.MODID)
+class ExampleMod(modEventBus: IEventBus?, modContainer: ModContainer?) {
     companion object {
-        const val MODID: String = "mod"
+        const val MODID: String = "examplemod"
         private val LOGGER: Logger = LogUtils.getLogger()
     }
 }
